6e838cafb9b9f398235f6db38b80e33c30c56b66
eray <relfishere@gmail.com>
2015-08-21 18:49:06 +0300

    davai

--- app.js
+++ app.js
@@ -77,18 +77,44 @@ app.get('/logout', utils.secure, function (req, res) {
 
 app.get('/:id', utils.secure, function (req, res) {
   utils.getUserInfo(utils.ctos(req.params.id), function (data) {
-    if (typeof data !== "undefined" &&
-        typeof data.response !== "undefined" &&
-        typeof data.response.players[0] !== "undefined") {
+    if (data !== false) {
+      utils.getFriendList(utils.ctos(req.params.id), function (list) {
+        var loop = list.length;
+        var friends_result = [];
 
-      data = data.response.players[0];
+        if (list.length === 0) {
+          res.render('profile', {
+            avatar: data.avatarfull,
+            date: moment(data.lastlogoff * 1000).fromNow(),
+            name: data.personaname,
+            location_str: data.loccountrycode + "/" + data.locstatecode,
+            profile_url: data.profileurl,
+            friends: [],
+            moment: moment,
+            utils: utils
+          });
+        }
 
-      res.render('profile', {
-        avatar: data.avatarfull,
-        date: moment(data.lastlogoff * 1000).fromNow(),
-        name: data.personaname,
-        location_str: data.loccountrycode + "/" + data.locstatecode,
-        profile_url: data.profileurl
+        for (var i = 0; i < list.length; i++) {
+          utils.getUserInfo(list[i].steamid, function (friend_info) {
+            if (friend_info !== false) {
+              friends_result.push(friend_info);
+            }
+
+            if (--loop === 0) {
+              res.render('profile', {
+                avatar: data.avatarfull,
+                date: moment(data.lastlogoff * 1000).fromNow(),
+                name: data.personaname,
+                location_str: data.loccountrycode + "/" + data.locstatecode,
+                profile_url: data.profileurl,
+                friends: friends_result,
+                moment: moment,
+                utils: utils
+              });
+            }
+          });
+        }
       });
     } else {
       res.render('error', { content: data });
--- libs/utils.js
+++ libs/utils.js
@@ -19,13 +19,43 @@ var ctos = function (c) {
 
 module.exports = {
   getUserInfo : function (id, callback) {
-    request('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+
+    request('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' +
       config.steamApiKey +
       '&steamids=' +
       id,
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          callback(JSON.parse(body));
+          var data = JSON.parse(body);
+
+          if (typeof data !== "undefined" &&
+              typeof data.response !== "undefined" &&
+              typeof data.response.players[0] !== "undefined") {
+            callback(data.response.players[0]);
+          } else {
+            callback(false);
+          }
+        } else {
+          callback(body);
+        }
+      }
+    );
+  },
+  getFriendList: function (id, callback) {
+    request('http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=' +
+      config.steamApiKey +
+      '&steamid=' + id +
+      '&relationship=friend',
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+
+          if (typeof data !== "undefined" &&
+              typeof data.friendslist !== "undefined" &&
+              typeof data.friendslist.friends !== "undefined") {
+            callback(data.friendslist.friends);
+          } else {
+            callback(false);
+          }
         } else {
           callback(body);
         }
