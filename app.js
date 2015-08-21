a959dda44020d37c5f3e23527d9dd0c00619ecb0
eray <relfishere@gmail.com>
2015-08-21 15:57:26 +0300

    Token Added.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var openid = require('openid');
 var moment = require('moment');
 var utils = require(__dirname + "/libs/utils");
 var config = require(__dirname + "/libs/config");
+var low = require('lowdb');
+var randomToken = require('rand-token');
+
+var db = new low('db.json', {autosave: true});
+global.users = db('users');
 var app = express();
 
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/views');
 app.use('/static', express.static(__dirname + '/static'));
@@ -13,7 +20,7 @@ var Steam = new openid.RelyingParty(config.verifyUrl, config.realm,
   true, true, []
 );
 
-app.get('/', function(req, res) {
+app.get('/', utils.ness, function(req, res) {
   res.render('login', {});
 });
 
@@ -40,17 +47,35 @@ app.get('/verify', function(req, res) {
         res.render('error', { content: 'Failed to authenticate user.' });
       } else {
         steam_id = result.claimedIdentifier.split('/')[5];
-        res.redirect('/' + utils.stoc(steam_id));
+
+        var done = users.find(function (obj) {
+          return obj.steam_id === steam_id;
+        });
+
+        var newToken = randomToken.generate(32);
+
+        if (typeof done !== "undefined") {
+          done.token = newToken;
+        } else {
+          users.push({
+            steam_id: steam_id,
+            token: newToken
+          });
+        }
+
+        res.cookie('token', newToken).redirect("/");
       }
     }
   });
 });
 
-app.get('/logout', function (req, res) {
-  res.redirect('/');
+app.get('/logout', utils.secure, function (req, res) {
+  var done = users.find({ steam_id: req.user.steam_id });
+  done.token = randomToken.generate(32);
+  res.clearCookie("token").redirect("/");
 });
 
-app.get('/:id', function (req, res) {
+app.get('/:id', utils.secure, function (req, res) {
   utils.getUserInfo(utils.ctos(req.params.id), function (data) {
     if (typeof data !== "undefined" &&
         typeof data.response !== "undefined" &&
@@ -61,7 +86,9 @@ app.get('/:id', function (req, res) {
       res.render('profile', {
         avatar: data.avatarfull,
         date: moment(data.lastlogoff * 1000).fromNow(),
-        name: data.personaname
+        name: data.personaname,
+        location_str: data.loccountrycode + "/" + data.locstatecode,
+        profile_url: data.profileurl
       });
     } else {
       res.render('error', { content: data });
--- libs/utils.js
+++ libs/utils.js
@@ -1,6 +1,22 @@
 var request = require("request");
 var config = require(__dirname + "/config");
 
+var stoc = function (s) {
+  var result = [];
+  for (var i = 0; i < s.length; i++) {
+    result.push(String.fromCharCode(parseInt(s[i]) + 97));
+  }
+  return result.join('');
+};
+
+var ctos = function (c) {
+  var result = [];
+  for (var i = 0; i < c.length; i++) {
+    result.push(c[i].charCodeAt(0) - 97);
+  }
+  return result.join('');
+};
+
 module.exports = {
   getUserInfo : function (id, callback) {
     request('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + config.steamApiKey + '&steamids=' + id, function (error, response, body) {
@@ -11,18 +27,31 @@ module.exports = {
       }
     });
   },
-  stoc: function (s) {
-    var result = [];
-    for (var i = 0; i < s.length; i++) {
-      result.push(String.fromCharCode(parseInt(s[i]) + 97));
+  stoc: stoc,
+  ctos: ctos,
+  secure: function (req, res, next) {
+    var done = users.find(function (obj) {
+      return obj.token === req.cookies.token;
+    });
+
+    if (typeof done === "undefined") {
+      res.clearCookie("token").redirect("/");
+    } else {
+      req.user = done;
+      next();
     }
-    return result.join('');
   },
-  ctos: function (c) {
-    var result = [];
-    for (var i = 0; i < c.length; i++) {
-      result.push(c[i].charCodeAt(0) - 97);
+  ness: function (req, res, next) {
+    var done = users.find(function (obj) {
+      return obj.token === req.cookies.token;
+    });
+
+    if (typeof done === "undefined") {
+      res.clearCookie("token");
+      next();
+    } else {
+      req.user = done;
+      res.redirect("/" + stoc(done.steam_id));
     }
-    return result.join('');
   }
 };
