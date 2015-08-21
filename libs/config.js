3faaefbc644cf39bbb311e80ea7c01705daad0ba
eray <relfishere@gmail.com>
2015-08-21 16:13:41 +0300

    Wut.

--- libs/config.js
+++ libs/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   steamOpenIdUrl : "http://steamcommunity.com/openid",
-  steamApiKey: "26DE8B5C9D296C796064E890F35FA5D0",
+  steamApiKey: "", // http://steamcommunity.com/dev/apikey
   verifyUrl: "http://localhost:3000/verify",
   realm: "http://localhost:3000/"
 };
