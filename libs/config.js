0bd4cc9f21dcda91186001389cdae2812b52bc82
eray <relfishere@gmail.com>
2015-08-24 16:17:20 +0300

    Update.

--- libs/config.js
+++ libs/config.js
@@ -1,6 +1,8 @@
+var base = "http://localhost";
+
 module.exports = {
   steamOpenIdUrl: 'http://steamcommunity.com/openid',
   steamApiKey: 'C9D5A6854C159FE5D8C321708C092ED2', // http://steamcommunity.com/dev/apikey
-  verifyUrl: 'http://localhost/verify',
-  realm: 'http://localhost/'
+  verifyUrl: base + '/verify',
+  realm: base + '/'
 };
